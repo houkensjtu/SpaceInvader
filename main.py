10b666554e88c4f0f6da2d65c4e0c17af30a373d
houkensjtu <houkensjtu@gmail.com>
2019-09-15 22:53:15 -0400

    Initial commit II.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 def run_game():
    pygame.init()
+   settings = Settings()
 
    # screen is a pygame.Surface object
-   screen = pygame.display.set_mode((640,480))
+   screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
    pygame.display.set_caption('Space Invader')
+
+   ship = Ship(screen)
 
    while True:
       for event in pygame.event.get():
@@ -15,6 +20,8 @@ def run_game():
 
       # pygame will not draw anything new before flip,
       # anything drawn will be buffered until flip() is called.
+      screen.fill(settings.bg_color)
+      ship.blitme()
       pygame.display.flip()
 
 run_game()
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+   def __init__(self):
+      """Initialize the game settings."""
+      self.screen_width  = 640
+      self.screen_height = 480
+      self.bg_color      = (230,230,230)
