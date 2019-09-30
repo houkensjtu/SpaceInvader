0bd2a0ea01c787bd7fc5c20b72e9aa2c4c473846
houkensjtu <houkensjtu@gmail.com>
2019-09-29 23:37:18 -0400

    Updated.

--- game_functions.py
+++ game_functions.py
@@ -1,12 +1,25 @@
 import sys
 import pygame
+from time import sleep
 
-def check_event():
-   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
+
+def check_event(ship):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
+
 
 def update_screen(settings, screen, ship):
-      screen.fill(settings.bg_color)
-      ship.blitme()
-      pygame.display.flip()
+    screen.fill(settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
--- main.py
+++ main.py
@@ -5,21 +5,25 @@ import game_functions as gf
 from settings import Settings
 from ship import Ship
 
+
 def run_game():
-   pygame.init()
-   settings = Settings()
+    pygame.init()
+    settings = Settings()
 
-   # screen is a pygame.Surface object
-   screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
-   pygame.display.set_caption('Space Invader')
-   
-   # Create a Space ship object.
-   ship = Ship(screen)
+    # screen is a pygame.Surface object
+    screen = pygame.display.set_mode(
+        (settings.screen_width, settings.screen_height))
+    pygame.display.set_caption('Space Invader')
 
-   while True:
-      gf.check_event()
-      # pygame will not draw anything new before flip,
-      # anything drawn will be buffered until flip() is called.
-      gf.update_screen()
+    # Create a Space ship object.
+    ship = Ship(screen)
+
+    while True:
+        gf.check_event(ship)
+        ship.update()
+        # pygame will not draw anything new before flip,
+        # anything drawn will be buffered until flip() is called.
+        gf.update_screen(settings, screen, ship)
+
 
 run_game()
